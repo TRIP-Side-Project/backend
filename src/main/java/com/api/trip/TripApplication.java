60a2f378ea0af80a4aca4d50221ee30f97480565
Dongmin Kim <ddmkim94@gmail.com>
2023-11-28 20:07:28 +0900

    프로젝트 생성
    - .gitignore 파일 작성

---
+++ src/main/java/com/api/trip/TripApplication.java
@@ -0,0 +1,13 @@
+package com.api.trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/trip/TripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
