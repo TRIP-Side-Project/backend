b69706075124e22d6d6bb5a90f17b8064f752977
Dongmin Kim <ddmkim94@gmail.com>
2023-11-30 13:51:49 +0900

    Feat: 회원 도메인 설계

---
+++ src/main/java/com/api/trip/domain/member/model/Member.java
@@ -0,0 +1,30 @@
+package com.api.trip.domain.member.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+}
