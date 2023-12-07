2e88e93761157c02782f0af00f7efaab6bcddcd0
kwondongwook <kwondongwook1@gmail.com>
2023-12-08 01:04:47 +0900

    Feat: 게시글 도메인 설계
    - Entity 추가
    - Repository 추가

---
+++ src/main/java/com/api/trip/domain/article/model/Article.java
@@ -0,0 +1,43 @@
+package com.api.trip.domain.article.model;
+
+import com.api.trip.domain.member.model.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member writer;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private long viewCount;
+
+    @Builder
+    private Article(Member writer, String title, String content, long viewCount) {
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+        this.viewCount = viewCount;
+    }
+}
