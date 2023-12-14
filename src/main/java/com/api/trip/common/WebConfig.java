2a5f7965c14ae89d83ebc3a9fbbec8a9ce87522b
kwondongwook <kwondongwook1@gmail.com>
2023-12-15 02:23:03 +0900

    Fix: WebConfig에서 @EnableWebMvc 제거
    - LocalDateTime 객체의 값이 배열 형태의 JSON으로 응답되는 버그 해결

--- src/main/java/com/api/trip/common/WebConfig.java
+++ src/main/java/com/api/trip/common/WebConfig.java
@@ -2,22 +2,19 @@ package com.api.trip.common;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
-    } //
-
+    }
 }
