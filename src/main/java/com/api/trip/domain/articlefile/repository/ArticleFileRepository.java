b0ab187c4d434f6c9d883f9ba72a4465506e3027
kwondongwook <kwondongwook1@gmail.com>
2023-12-18 20:39:05 +0900

    Feat: 게시글 등록, 수정, 삭제 시 ArticleFile 관련 로직 추가
    - article 테이블의 content 컬럼 타입을 varchar에서 text로 변경
    - URL 추출 유틸 UrlExtractor 추가

--- src/main/java/com/api/trip/domain/articlefile/repository/ArticleFileRepository.java
+++ src/main/java/com/api/trip/domain/articlefile/repository/ArticleFileRepository.java
@@ -1,7 +1,25 @@
 package com.api.trip.domain.articlefile.repository;
 
+import com.api.trip.domain.article.model.Article;
 import com.api.trip.domain.articlefile.model.ArticleFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArticleFileRepository extends JpaRepository<ArticleFile, Long> {
+
+    @Modifying
+    @Query("UPDATE ArticleFile af SET af.article = :article WHERE af.article IS NULL AND af.url IN :urls")
+    void setArticleWhereArticleNullAndUrlIn(@Param("article") Article article, @Param("urls") List<String> urls);
+
+    @Modifying
+    @Query("UPDATE ArticleFile af SET af.article = null WHERE af.article = :article AND af.url NOT IN :urls")
+    void setArticleNullWhereArticleAndUrlNotIn(@Param("article") Article article, @Param("urls") List<String> urls);
+
+    @Modifying
+    @Query("UPDATE ArticleFile af SET af.article = null WHERE af.article = :article")
+    void setArticleNullWhereArticle(@Param("article") Article article);
 }
