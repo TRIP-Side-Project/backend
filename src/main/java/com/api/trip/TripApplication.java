fce8ee451fc52f25b625bafac71174583261a91d
seungnYong <77851079+gkfktkrh153@users.noreply.github.com>
2023-12-19 16:03:32 +0900

    Update TripApplication.java

--- src/main/java/com/api/trip/TripApplication.java
+++ src/main/java/com/api/trip/TripApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TripApplication {
+public class TripApplication { //
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripApplication.class, args);
