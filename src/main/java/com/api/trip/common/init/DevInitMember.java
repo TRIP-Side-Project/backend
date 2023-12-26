ffcf686cb58d9033d4d8338959bcb322a43e90ca
Dongmin Kim <ddmkim94@gmail.com>
2023-12-26 17:37:57 +0900

    Feat: 개발용 회원 데이터 추가
    - 개발 환경에서 사용할 회원 데이터 2개 생성

---
+++ src/main/java/com/api/trip/common/init/DevInitMember.java
@@ -0,0 +1,35 @@
+package com.api.trip.common.init;
+
+import com.api.trip.domain.member.model.Member;
+import com.api.trip.domain.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Profile("dev")
+public class DevInitMember {
+
+    @Value("${cloud.aws.default-image}")
+    private String defaultProfileImg;
+
+    @Bean
+    public CommandLineRunner init(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            memberRepository.save(createMember("trip123@test.com", "trip_member", passwordEncoder.encode("trip1234")));
+            memberRepository.save(createMember("admin@test.com", "admin", passwordEncoder.encode("trip1234")));
+        };
+    }
+
+    private Member createMember(String email, String nickname, String password) {
+        return Member.builder()
+                .email(email)
+                .nickname(nickname)
+                .password(password)
+                .profileImg(defaultProfileImg)
+                .build();
+    }
+}
