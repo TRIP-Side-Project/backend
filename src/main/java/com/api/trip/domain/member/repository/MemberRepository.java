84152ea518a3f77d2aef3502dcbab7cc713bc2b6
Dongmin Kim <ddmkim94@gmail.com>
2024-01-04 14:53:08 +0900

    Refactor: 이메일 전송 기능 리팩토링

--- src/main/java/com/api/trip/domain/member/repository/MemberRepository.java
+++ src/main/java/com/api/trip/domain/member/repository/MemberRepository.java
@@ -1,10 +1,14 @@
 package com.api.trip.domain.member.repository;
 
 import com.api.trip.domain.member.model.Member;
+import com.api.trip.domain.member.model.SocialCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+    
+    Optional<Member> findByEmailAndSocialCode(String email, SocialCode socialCode);
+
 }
