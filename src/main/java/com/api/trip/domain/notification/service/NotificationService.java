cc5d6a8c3d98d54e3c76661b71f850ea6ff4ed25
kwondongwook <kwondongwook1@gmail.com>
2024-01-07 14:38:43 +0900

    Feat: 오타 수정

--- src/main/java/com/api/trip/domain/notification/service/NotificationService.java
+++ src/main/java/com/api/trip/domain/notification/service/NotificationService.java
@@ -29,10 +29,10 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final InterestTagService interestTagService;
 
+    @Transactional(readOnly = true)
     public Long getMemberId(String email) {
-        Member member = memberRepository.findByEmail(email)
-                .orElseThrow(() -> new CustomException(ErrorCode.UNAUTHORIZED));
-        return member.getId();
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(ErrorCode.UNAUTHORIZED)).getId();
     }
 
     public Set<Long> createNotifications(Item item, List<String> tagNames) {
